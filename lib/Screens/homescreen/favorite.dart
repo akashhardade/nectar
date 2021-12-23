1ffd54d9c4ae9eea23f285f745e0cb066ab9d139
akash <akashhardade97@gmail.com>
2021-12-23 23:31:36 +0530

    favourite page added

--- lib/Screens/homescreen/favorite.dart
+++ lib/Screens/homescreen/favorite.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Favorite extends StatefulWidget {
   Favorite({Key? key}) : super(key: key);
 
@@ -10,7 +11,74 @@ class _FavoriteState extends State<Favorite> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        toolbarHeight: 80,
+        title: Text("Favourite",
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 25)),
+        elevation: 1,
+      ),
+      body: ListView.builder(
+          itemCount: 2,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              child: Container(
+                height: 110,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                          color: Colors.grey.withOpacity(0.5), width: 0.5)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [ 
+                    Image(
+                      height: 100,
+                      width: 100,
+                      image: AssetImage("assets/images/b4.png"),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "pepsi",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text("1 pcs price"),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 50,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("\$ 2.50",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.keyboard_arrow_right))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
     );
   }
 }
