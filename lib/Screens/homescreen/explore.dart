ec1123bff872f0e8d1ed8dc4fc60bc9a64240151
akash <akashhardade97@gmail.com>
2021-12-23 18:15:37 +0530

    explore and ccategoryproductlist page done

--- lib/Screens/homescreen/explore.dart
+++ lib/Screens/homescreen/explore.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:grocery/Screens/homescreen/categoryproductlist.dart';
+import 'package:grocery/constant/Data.dart';
+
 class Explore extends StatefulWidget {
   Explore({Key? key}) : super(key: key);
 
@@ -6,11 +9,122 @@ class Explore extends StatefulWidget {
   _ExploreState createState() => _ExploreState();
 }
 
+ int currentindex = 0;
+  List<int> color = [
+    0xff53B175,
+    0xffF8A44C,
+    0xffB7DFF5,
+    0xff53B175,
+    0xffF8A44C,
+    0xff53B175,
+  ];
 class _ExploreState extends State<Explore> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-     
-    );
-  }
+    return  Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+            child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Find Products",
+                style: TextStyle(
+                    fontFamily: "Gilroy-ExtraBold",
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.90,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 45,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(20)),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.black,
+                          ),
+                          filled: true,
+                          fillColor: Colors.black12,
+                          hintText: "Search Store",
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      child: GridView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          gridDelegate:
+                              const SliverGridDelegateWithMaxCrossAxisExtent(
+                                  maxCrossAxisExtent: 200,
+                                  crossAxisSpacing: 20,
+                                  mainAxisSpacing: 20),
+                          itemCount: categories.length,
+                          itemBuilder: (BuildContext ctx, index) {
+                            return InkWell(
+                              onTap: () {
+                                setState(() {
+                                  currentindex = index;
+                                });
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (_) => CategoryProductList(products:  categories[currentindex],) ));
+                              },
+                              child: Container(
+                                alignment: Alignment.center,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 11),
+                                  child: Column(
+                                    //   mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Image(
+                                          height: 100,
+                                          width: 100,
+                                          image: AssetImage(
+                                              "${categories[index]["image"]}")),
+                                      Text(
+                                        "${categories[index]["category"]}",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Color(color[index]).withOpacity(0.1),
+                                  borderRadius: BorderRadius.circular(15),
+                                  border: Border.all(
+                                    color: Color(color[index]),
+                                  ),
+                                ),
+                              ),
+                            );
+                          }),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )),
+      ),
+    );  }
 }
---
+++ lib/constant/Data.dart
@@ -0,0 +1,192 @@
+List<Map> categories = [
+  {
+    "category": "Fresh fruit & vegetables",
+    "image": "assets/images/fruit.png",
+    "subcategory": [
+      {
+        "id": 1,
+        "title": "Organic Bananas",
+        "image": "assets/images/banana.png",
+        "price": "4.99",
+        "description": " 7pcs ,Price",
+      },
+      {
+        "id": 2,
+        "title": "Red Apple",
+        "image": "assets/images/apple.png",
+        "price": "4.99",
+        "description": "1 Kg ,Price",
+      },
+      {
+        "id": 3,
+        "title": "Strawberry",
+        "image": "assets/images/banana.png",
+        "price": "5.99",
+        "description": "12 pcs ,Price",
+      },
+      {
+        "id": 4,
+        "title": "Red Capsicum",
+        "image": "assets/images/bellpaper.png",
+        "price": "8.56",
+        "description": "6 pcs,Price",
+      },
+      {
+        "id": 5,
+        "title": "Ginger",
+        "image": "assets/images/ginger.png",
+        "price": "9.99",
+        "description": "8 pcs,Price",
+      },
+      {
+        "id": 6,
+        "title": "cucumber",
+        "image": "assets/images/banana.png",
+        "price": "4.99",
+        "description": "3 pcs,Price",
+      },
+    ],
+  },
+  {
+    "category": "Cooking oil and ghee",
+    "image": "assets/images/oil.png",
+    "subcategory": [
+      {
+        "id": 7,
+        "title": "Red Capsicum",
+        "image": "assets/images/bellpaper.png",
+        "price": "8.56",
+        "description": "6 pcs,Price",
+      },
+      {
+        "id": 8,
+        "title": "Ginger",
+        "image": "assets/images/ginger.png",
+        "price": "9.99",
+        "description": "8 pcs,Price",
+      },
+      {
+        "id": 9,
+        "title": "cucumber",
+        "image": "assets/images/banana.png",
+        "price": "4.99",
+        "description": "3 pcs,Price",
+      },
+    ]
+  },
+  {
+    "category": "Meat and fish",
+    "image": "assets/images/meat.png",
+    "subcategory": [
+      {
+        "id": 10,
+        "title": "Beef Bone",
+        "image": "assets/images/beef.png",
+        "price": "4.56",
+        "description": "6 pcs,Price",
+      },
+      {
+        "id": 11,
+        "title": "Boiler Chicken",
+        "image": "assets/images/chicken.png",
+        "price": "9.99",
+        "description": "8 pcs,Price",
+      },
+      {
+        "id": 12,
+        "title": "Fish",
+        "image": "assets/images/chicken.png",
+        "price": "4.99",
+        "description": "3 pcs,Price",
+      },
+    ]
+  },
+  {
+    "category": "Bakery and snacks",
+    "image": "assets/images/bakery.png",
+    "subcategory": [
+      {
+        "id": 13,
+        "title": "Red Capsicum",
+        "image": "assets/images/bellpaper.png",
+        "price": "8.56",
+        "description": "6 pcs,Price",
+      },
+      {
+        "id": 14,
+        "title": "Ginger",
+        "image": "assets/images/ginger.png",
+        "price": "9.99",
+        "description": "8 pcs,Price",
+      },
+      {
+        "id": 15,
+        "title": "cucumber",
+        "image": "assets/images/banana.png",
+        "price": "4.99",
+        "description": "3 pcs,Price",
+      },
+    ]
+  },
+  {
+    "category": "daily eggs",
+    "image": "assets/images/eggs.png",
+    "subcategory": [
+      {
+        "id": 16,
+        "title": "Red Capsicum",
+        "image": "assets/images/egg1.png",
+        "price": "8.56",
+        "description": "6 pcs,Price",
+      },
+      {
+        "id": 17,
+        "title": "Ginger",
+        "image": "assets/images/egg2.png",
+        "price": "9.99",
+        "description": "8 pcs,Price",
+      },
+      {
+        "id": 18,
+        "title": "cucumber",
+        "image": "assets/images/egg3.png",
+        "price": "4.99",
+        "description": "3 pcs,Price",
+      },
+    ]
+  },
+  {
+    "category": "Bevrages",
+    "image": "assets/images/bevrages.png",
+    "subcategory": [
+      {
+        "id": 19,
+        "title": "limbu sarbat",
+        "image": "assets/images/b1.png",
+        "price": "8.56",
+        "description": "6 pcs,Price",
+      },
+      {
+        "id": 20,
+        "title": "red wine",
+        "image": "assets/images/b2.png",
+        "price": "9.99",
+        "description": "8 pcs,Price",
+      },
+      {
+        "id": 21,
+        "title": "sprite",
+        "image": "assets/images/b3.png",
+        "price": "4.99",
+        "description": "3 pcs,Price",
+      },
+       {
+        "id": 22,
+        "title": "coco cola",
+        "image": "assets/images/b4.png",
+        "price": "4.99",
+        "description": "3 pcs,Price",
+      },
+    ]
+  }
+];
